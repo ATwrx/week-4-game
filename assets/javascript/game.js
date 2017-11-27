e9d7afcb10b7f3ed0a83da92f5449dc31258d817
A. Thompson <act.webdesigns@gmail.com>
2017-11-27 16:21:50 -0600

    IT WORKS!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,42 @@
-$( document ).ready(function(){
-  var wins = 0;
-  var losses = 0;
-  var randomNumber;
+var wins = 0;
+var losses = 0;
+var randomNumber = 0;
+var scoreCounter = 0;
+// Assign random number to guees & begin the game
+function getRandomNumbers() {
+  scoreCounter = 0;
+  $("#score-area").html(scoreCounter);
+  randomNumber = Math.floor(Math.random() * 100 + 20);
+  $("#random-number").html(randomNumber);
+  $("#c1").attr("value", getCrystalNumbers());
+  $("#c2").attr("value", getCrystalNumbers());
+  $("#c3").attr("value", getCrystalNumbers());
+  $("#c4").attr("value", getCrystalNumbers());
+}
 
-  function randomNumber() {
-    var randomNum = Math.floor(Math.random() * 100) + 20;
-    randomNumber = randomNum;
-    $("#random-number").html() = randomNumber;
-    return randomNumber;
+function getCrystalNumbers() {
+  var crystalVal = Math.floor(Math.random() * 12 + 1);
+  return crystalVal;
+}
+
+function updateScore() {
+  if (scoreCounter < randomNumber) {
+    return;
+  } else if (scoreCounter > randomNumber) {
+    losses++;
+    $("#losses").text(losses);
+    getRandomNumbers();
+  } else {
+    wins++;
+    $("#wins").text(wins);
+    getRandomNumbers();
   }
+}
 
-  function getCrystalNumbers() {
-
-  }
-
-  function updateScore() {
-
-  }
-
-
-})
+$(".crystal").click(function() {
+  var num = $(this).val();
+  scoreCounter += parseInt(num);
+  $("#score-area").html(scoreCounter);
+  updateScore();
+  console.log(num);
+});
