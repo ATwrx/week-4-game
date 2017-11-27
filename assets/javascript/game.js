7c89fe37b354718e3bafd1aac400a5bc4998826f
A. Thompson <act.webdesigns@gmail.com>
2017-11-27 13:42:17 -0600

    only need bg to finish styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+$( document ).ready(function(){
+  var wins = 0;
+  var losses = 0;
+  var randomNumber;
+
+  function randomNumber() {
+    var randomNum = Math.floor(Math.random() * 100) + 20;
+    randomNumber = randomNum;
+    $("#random-number").html() = randomNumber;
+    return randomNumber;
+  }
+
+  function getCrystalNumbers() {
+
+  }
+
+  function updateScore() {
+
+  }
+
+
+})
